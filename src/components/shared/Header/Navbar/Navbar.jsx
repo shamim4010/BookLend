34e443143994bdc7f82ca19273dbd04abe28ff35
shamim4010 <shamimislam401017@gmail.com>
2026-05-01 18:35:10 +0600

    feat: integrate Google login functionality and add animate.css for enhanced UI effects

--- src/components/shared/Header/Navbar/Navbar.jsx
+++ src/components/shared/Header/Navbar/Navbar.jsx
@@ -6,18 +6,19 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 import { FaBookOpen, FaHome, FaRegUserCircle } from 'react-icons/fa'
 import { GiSpellBook } from 'react-icons/gi'
+import 'animate.css';
 
 function Navbar() {
     const pathName = usePathname()
-    const { data: session } = authClient.useSession()
+    const { data: session, isPending } = authClient.useSession()
     console.log(session)
 
     const userInfo = session?.user;
 
     const liLinks = <>
-        <li><Link href='/' className={pathName === '/' ? 'text border-b-2 border-[#2f2ebe] navAnimat': ''}><FaHome /> Home</Link></li>
-        <li><Link href='/allbooks' className={pathName === '/allbooks' ? 'text border-b-2 border-[#2f2ebe] navAnimat': ''}><FaBookOpen /> All Books</Link></li>
-        <li><Link href='/profile' className={pathName === '/profile' ? 'text border-b-2 border-[#2f2ebe] navAnimat': ''}><FaRegUserCircle /> My Profile</Link></li>
+        <li><Link href='/' className={pathName === '/' ? 'text border-b-2 border-[#2f2ebe] navAnimat rounded-none': ''}><FaHome /> Home</Link></li>
+        <li><Link href='/allbooks' className={pathName === '/allbooks' ? 'text border-b-2 border-[#2f2ebe] navAnimat rounded-none': ''}><FaBookOpen /> All Books</Link></li>
+        <li><Link href='/profile' className={pathName === '/profile' ? 'text border-b-2 border-[#2f2ebe] navAnimat rounded-none': ''}><FaRegUserCircle /> My Profile</Link></li>
     </>
 
     return (
@@ -33,7 +34,7 @@ function Navbar() {
                             {liLinks}
                     </ul>
                 </div>
-                <Link href="/" className="text text-2xl flex items-center gap-2 cursor-pointer drop-shadow-[0_0_12px_rgba(47,46,190,1)]"><GiSpellBook/> BookLend</Link>
+                <Link href="/" className="text text-2xl flex items-center gap-2 cursor-pointer active:animate__fadeIn drop-shadow-[0_0_12px_rgba(47,46,190,1)]"><GiSpellBook/> BookLend</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -41,7 +42,7 @@ function Navbar() {
                 </ul>
             </div>
             <div className="navbar-end">
-                {userInfo ? <div className='flex justify-center items-center gap-4'><div className='w-10 h-10 flex justify-center items-center overflow-hidden border-2 border-indigo-500 rounded-full'><Image src={userInfo.image} width={100} height='100' alt={userInfo.name} /></div> <button className='btn border-none bg text-white '>LogOut</button></div> : <Link href="/auth/login" className="btn btn-sm bg border-none text-white rounded-sm">Login</Link>}
+                {isPending ? 'Loading': userInfo ? <div className='flex justify-center items-center gap-4'><div className='w-10 h-10 flex justify-center items-center overflow-hidden border-2 border-indigo-500 rounded-full'><Image src={userInfo.image} width={100} height='100' alt={userInfo.name} /></div> <button onClick={async () => await authClient.signOut()} className='btn border-none bg text-white '>LogOut</button></div> : <Link href="/auth/login" className="btn btn-sm bg border-none text-white rounded-sm">Login</Link>}
             </div>
         </div>
     )
