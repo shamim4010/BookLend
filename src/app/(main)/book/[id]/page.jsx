3ec4ff5d661db4636411a2cdf9d98d002c1bf180
shamim4010 <shamimislam401017@gmail.com>
2026-05-02 10:00:02 +0600

    feat: enhance BookDetails component to accept params for dynamic book ID handling

--- src/app/(main)/book/[id]/page.jsx
+++ src/app/(main)/book/[id]/page.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-function BookDetails() {
-  return (
-    <div>BookDetails</div>
-  )
+function BookDetails({ params }) {
+    const name = params;
+    console.log(name.id)
+    let names = '';
+    /* for (let i = 0; i < name.id.length; i++){
+      console.log(name.id[i]);
+      if (name.id[i] === '%' ){
+        names += ' ';
+        i+=2
+      }
+      else{
+        names += name.id[i];
+      }
+    }
+    console.log(names); */
+
+    return (
+        <div>BookDetails</div>
+    )
 }
 
 export default BookDetails
